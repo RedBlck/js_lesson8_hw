81b362a32ad699d996c5f3fdcba6b98100fc2655
RedBlck <pentsakav@gmail.com>
2021-12-11 00:10:02 +0200

    Initial commit

---
+++ main.js
@@ -0,0 +1,38 @@
+// - Напишіть код,  котрий :
+// -- отримує текст з параграфа з id "content"
+// let content = document.getElementById('content');
+// console.log(content.innerText);
+
+
+// -- отримує текст з блоку з id "rules"
+// let rules = document.getElementById('rules');
+// console.log(rules.innerText);
+
+
+// -- замініть текст параграфа з id 'content' на будь-який інший
+// content.innerText = 'тест завдання';
+// console.log(content.innerText);
+
+
+// -- замініть текст параграфа з id 'rules' на будь-який інший
+// rules.innerText = 'другий тест завдання';
+// console.log(rules.innerText);
+
+
+// -- змініть кожному елементу колір фону на червоний
+// -- змініть кожному елементу колір тексту на синій
+// let elements = document.body.children;
+// for (const element of elements) {
+//     element.style.background = 'red';
+//     element.style.color = 'blue';
+// }
+
+// -- отримати весь список класів елемента з id=rules і вивести їх в console.log
+// console.log(document.getElementById('rules').classList);
+
+
+// -- поміняти колір тексту у всіх елементів fc_rules на червоний
+// let rulesElements = document.getElementsByClassName('fc_rules')
+// for (const rulesElement of rulesElements) {
+//     rulesElement.style.color = 'red';
+// }
